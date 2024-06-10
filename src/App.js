d8c806aaeb0e4f5eb7e91952f8e0bec50afc5c17
Brian <briantechdom@gmail.com>
2024-06-10 15:29:30 -0700

    hide initial arrow states

--- src/App.js
+++ src/App.js
@@ -25,17 +25,17 @@ function App() {
 
   return (
     // parent div container
-    <div className='max-width-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
+    <div className='max-width-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
       {/* banner container */}
       <div style={{backgroundImage: `url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
     
     {/* left arrow */}
-    <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> 
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> 
       <BsChevronCompactLeft size={30}/>
 
     </div>
     {/* right arrow */}
-    <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> 
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> 
       <BsChevronCompactLeft size={30}/>
 
     </div>
