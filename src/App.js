7be9ec8e86bd4f54c4f9413100bfe098fd4519d7
Brian <briantechdom@gmail.com>
2024-06-10 15:32:48 -0700

    Set the image's slide index to the currentIndex variable

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     // parent div container
     <div className='max-width-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
       {/* banner container */}
-      <div style={{backgroundImage: `url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
+      <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
     
     {/* left arrow */}
     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> 
